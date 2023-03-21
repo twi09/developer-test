4942f0939eed46c38cfde4af458cd6baafe75418
twi09 <victor.guyomard@irisa.fr>
2023-03-21 22:06:39 +0100

    End the implementation with the method that cut non valid solutions and sort probabilities in a post-hoc fashion

--- toy_problem.py
+++ toy_problem.py
@@ -1,5 +1,4 @@
 import numpy as np 
-
 '''
 This script aim to understand the problem on a toy graph
 '''
@@ -7,15 +6,13 @@ This script aim to understand the problem on a toy graph
 inf = float("inf")
 
 # Example of graph 
-G = np.array([[1,6,6,inf],[inf,1,1,4],[inf,inf,1,1],[inf,inf,inf,1]])
+G = np.array([[1,6,6,inf],[inf,1,1,4],[inf,inf,1,1],[inf,inf,inf,inf]])
 
 # Autonomy of  Millennium Falcon
 autonomy = 6
 # Deadline annihilation
 countdown = 7 
 
-# List of tuples (planet,day)
-Bounty_hunters = [("Hoth",6),("Hoth",7),("Hoth",8)]
 
 # Dictionary that map planets to index in G 
 dico_planet_to_index = {"Tatouine":0,"Dagobah":1,"Hoth":2,"Endor":3}
@@ -27,52 +24,118 @@ dico_index_to_planet = {v: k for k, v in dico_planet_to_index.items()}
 Write a script that enumerate all the paths from Tatoine to Endor and filter based on a condition
 '''
 
-# Remove paths based in Paths_in_progress on a condition 
-# To test: Condition = endor visited or cycle of size 3 
-def test_remove(paths,Return_path,Trash) :
-    # Visit all the possible paths and remove pathes that not match condition 
+# Compute the probability from the total number of time meet bounty hunters 
+compute_probability = lambda nb_total_meet : sum(((9**(k-1))/ 10**(k)) for k in range(1,nb_total_meet+1)) if nb_total_meet!=0 else 0
+
+
+
+# Remove paths that contains endor in the path in progess
+def remove_valid(paths,Return_path) :
+    # Visit all the possible paths and remove valid solutions from path in progress
     copy_paths = paths.copy()
-    for e in copy_paths:
-        # Number of duplicate values in a path
-        unique,counts = np.unique(e,return_counts=True)
-        if ("Endor" in e) :
-            paths.remove(e)
-            Return_path.append(e)
+    for i in range(len(copy_paths)):
+        if ("Endor" in copy_paths[i][0]) :
+            paths.remove(copy_paths[i])
+            Return_path.append(copy_paths[i])
         
-        elif 3 in counts :
-            paths.remove(e)
-            Trash.append(e)
 
+# Compute valid solutions from a current path 
+def compute_new_paths(current_path,neight_nodes,costs_nh,autonomy_default,countdown,bounty_hunters) :     
+    # Store the valid paths 
+    new_path = []
+    # test all the possibe neighbors
+    for i in range(len(neight_nodes)) :
+        # if enough autonomy an before countdown, travel and autonomy = autonomy - nb_day_to_travel
+        if current_path[1][0] >=  costs_nh[i]  and current_path[1][1] + costs_nh[i] <= countdown : 
+            autonomy = current_path[1][0] - costs_nh[i] 
+            # number of days = number of days + time to travel 
+            nb_of_days = current_path[1][1]+costs_nh[i]
+            # if nb_of_days correponds to a number of day where bounty hunters are in a given planet 
+            if nb_of_days in bounty_hunters.keys() : 
+                # Iterate number of time meet bounty hunters (+1 if meet else 0)
+                nb_bounty_meet = current_path[1][2]+ int(bounty_hunters[int(nb_of_days)] == neight_nodes[i])
+            else : 
+                # nothing to meet ! 
+                nb_bounty_meet = current_path[1][2] 
+            # if travel on the same planet (=wait on the planet)
+            if neight_nodes[i] == current_path[0][-1] :
+                # Then back to default autonomy 
+                autonomy = autonomy_default
+            new_path.append([current_path[0] + [ neight_nodes[i] ], [autonomy,nb_of_days,nb_bounty_meet]])
+            
+        
+            
+    # if empty autonomy and before countdown then need to refuel
+    if current_path[1][0]==0 and current_path[1][1] +  1 <= countdown : 
+        # number of days = number of days + time to refuel (=1)
+        nb_of_days = current_path[1][1]+ 1
+        # autonomy is reset to default 
+        autonomy = autonomy_default 
+        # if nb_of_days correponds to a number of day where bounty hunters are in a given planet 
+        if nb_of_days in bounty_hunters.keys() : 
+            # Iterate number of time meet bounty hunters (+1 if meet else 0)
+            nb_bounty_meet = current_path[1][2]+ int(bounty_hunters[int(nb_of_days)] == current_path[0][-1])
+        else : 
+            # nothing to meet ! 
+            nb_bounty_meet = current_path[1][2] 
+        # Append only the refuel solution (stay on the same planet = repeat the last node of the path)
+        new_path.append([current_path[0] + [ current_path[0][-1] ], [autonomy,nb_of_days,nb_bounty_meet]])
 
+    return(new_path)
 
-# List with current paths, init with the first node
-Paths_in_progress=[["Tatouine"]]
+# Autonomy of  Millennium Falcon
+autonomy_default = 6
+# Deadline annihilation
+countdown = 10
+
+# nb_of day as init
+nb_of_days_init = 0 
+# count number of time meet bounty_hunters init
+nb_bounty_meet_init = 0 
+
+# List with current paths, a path is defined as follow: [[nodes1,nodes2,...],[autonomy,nb_of_days,nb_bounty_meet]], init with the first node
+Paths_in_progress=[[["Tatouine"],[autonomy_default,nb_of_days_init,nb_bounty_meet_init]]]
 # List that contains the valid solutions 
 Return_path = []
-# List of invalid paths
-Trash = []
+# Dictionnary of day:planet for bounty hunters 
+bounty_hunters = {6:"Hoth",7:"Hoth",8:"Hoth"}
 
-# Test on a small number of iter 
-for i in range(18) :
+
+while Paths_in_progress!=[]  : 
     # Take the path of Paths_in_progress
     current_path =  Paths_in_progress.pop(0)
     # Take the last node of the path 
-    last_node = current_path[-1]
+    last_node = current_path[0][-1]
     
-    # Costs to add a node that is in the neighborhood of last_node
+    
+    # Costs to add a node that is in the neighborhood of last_node (+remove float_inf cost nodes)
     costs_nh = G[dico_planet_to_index[last_node]]
     
+    # Select index of non-inf nodes 
+    index_non_inf = np.where(costs_nh!=float("inf"))[0]
     # List of neighborhood nodes (non float_inf cost nodes)
-    neight_nodes = [dico_index_to_planet[i] for i in np.where(costs_nh!=float("inf"))[0]]
+    neight_nodes = [dico_index_to_planet[i] for i in index_non_inf]
     
-    # New paths obtained by adding neighborhood nodes
-    new_paths = [current_path + [neight_nodes[i]] for i in range(len(neight_nodes))]
+    # Filter non-inf costs 
+    costs_nh = costs_nh[index_non_inf]
     
-    # Remove invalid nodes and valid nodes in new_paths
-    test_remove(new_paths,Return_path,Trash)
- 
+    # New paths obtained from current_path that are before countdown 
+    new_paths = compute_new_paths(current_path,neight_nodes,costs_nh,autonomy_default,countdown,bounty_hunters)
     
-    # Add remaining path
+    # Add new paths to progess paths list
     Paths_in_progress +=new_paths
     
+    # Remove valid solutions in progress paths list
+    remove_valid(Paths_in_progress,Return_path)
+ 
+# If not solutions 
+if Return_path==[] : 
+    print("The probability of sucess is",0)
+else : 
+    # Take path with minimum number of time meet bounty hunters 
+    nb_min_meet = min(Return_path,key=lambda x:x[1][2])[1][2]
+    print("The probability of sucess is :", 1-compute_probability(nb_min_meet))
+
+
+
 
